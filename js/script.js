f52e060a7f37c0a728d6759e3ff2886a80938f4a
imranalam818 <70557619+imranalam818@users.noreply.github.com>
2020-11-23 23:53:20 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 function init(){
 //add your javascrip between these two lines of code
-	function sample() {
-		console.log ('Welcome');
-	} 
-	const ;
+	var d4 = document.getElementById('entrybutton');
+	d4.addEventListener('click' , myEventFunction1);
+	d4.addEventListener('click' , myEventFunction2);
 
+	function myEventFunction1 () {
+		var fc = document.getElementById('entryinput');
+		alert('Imran Alam: '+fc.value);
+	}
 
+	function myEventFunction2 () {
+	var fc = document.getElementById('entryinput');
+	var h2 = document.getElementById('textoutput');
+	h2.innerHTML = fc.value;
+	}
 
-
-
-
-
-
+}	
 
 window.addEventListener('load', init);
